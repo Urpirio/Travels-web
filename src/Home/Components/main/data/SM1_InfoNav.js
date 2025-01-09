e5ae56ba7d5e7829d7c80e4ecbd0fd539c8fe3a3
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-09 17:26:22 -0400

    Datos de la section 1

---
+++ src/Home/Components/main/data/SM1_InfoNav.js
@@ -0,0 +1,19 @@
+const InfoNav = [
+    {
+        link: "",
+        name: "Home",
+    },
+    {
+        link: "",
+        name: "about",
+    },
+    {
+        link: "",
+        name: "Hot tours",
+    },
+    {
+        link: "",
+        name: "Contact",
+    },
+];
+export default InfoNav;

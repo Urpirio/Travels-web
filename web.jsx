e30f78e744d30ae12b1e93ac8ffb87b5dc589479
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-09 17:30:55 -0400

    Componente principal encargado del renderizado de la pagina

---
+++ web.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Home from "./src/Home";
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+<Home/>
+    </StrictMode>
+);

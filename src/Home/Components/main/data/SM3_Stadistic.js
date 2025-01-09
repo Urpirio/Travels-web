0f794a6b2bbfa770b4447d8648c1e589bcf45b28
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-09 17:27:22 -0400

    Datos de la Section 3

---
+++ src/Home/Components/main/data/SM3_Stadistic.js
@@ -0,0 +1,19 @@
+const Stats_Name = [
+    {
+        Stats: "500",
+        name: "Lorem",
+    },
+    {
+        Stats: "10",
+        name: "Years",
+    },
+    {
+        Stats: "5000",
+        name: "Members",
+    },
+    {
+        Stats: "550",
+        name: "Lorem",
+    },
+]
+export default Stats_Name;

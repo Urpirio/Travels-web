1a3fded26f33f160519ed9f4cb83a555653995d4
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-09 17:30:25 -0400

    Componentee que resive todos los subcomponente Section para luego ser enviados y renderizados en el index y con webl.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,21 @@
+import SectionMain1 from "./Home/Components/main/SectionMain1";
+import SectionMain2 from "./Home/Components/main/SectionMain2";
+import SectionMain3 from "./Home/Components/main/SectionMain3";
+const Home = () =>{
+    return(
+        <section>
+            <header>
+
+            </header>
+            <main>
+                <SectionMain1/>
+                <SectionMain2/>
+                <SectionMain3/>
+            </main>
+            <footer>
+
+            </footer>
+        </section>
+    )
+};
+export default Home;

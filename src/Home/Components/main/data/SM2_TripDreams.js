d86785492c8143272b8be86996fd2bef1c8eff76
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-09 17:26:59 -0400

    Datos de la section 2

---
+++ src/Home/Components/main/data/SM2_TripDreams.js
@@ -0,0 +1,18 @@
+const TripDreams = [
+    {
+        content: "5 day and nights in a hotel with a magical things",
+    },
+    {
+        content: " ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .",
+    },
+    {
+        content:" Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+    },
+    {
+        content: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+    },
+    {
+        content: "incididunt ut labore et dolore magna aliqua",
+    },
+];
+export default TripDreams;

c5ae0536c90c910a5378b85ce9b9ffc76b8b5e16
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-11 21:29:10 -0400

    Se agrego los datos de la Section 4

---
+++ src/Home/Components/main/data/SM4_Scroll.js
@@ -0,0 +1,46 @@
+const CardScroll = [
+    {
+        image: "src/Home/Assets/main/Scroll/alpine-4553488_1280.jpg",
+        alt: "Alpine img",
+        link: "",
+        place: "Title",
+        price: "1100 $"
+    },
+    {
+        image: "src/Home/Assets/main/Scroll/alps-4549188_1280.jpg",
+        alt: "Alps img",
+        link: "",
+        place: "Title",
+        price: "1500 $"
+    },
+    {
+        image: "src/Home/Assets/main/Scroll/mountain-970704_1280.jpg",
+        alt: "Mountain img",
+        link: "",
+        place: "Title",
+        price: "1200 $"
+    },
+    {
+        image: "src/Home/Assets/main/Scroll/mountain-7866362_1280.jpg",
+        alt: "Mountain img",
+        link: "",
+        place: "Title",
+        price: "1000 $"
+    },
+    {
+        image: "src/Home/Assets/main/Scroll/mountains-5581991_1280.jpg",
+        alt: "Mountain img",
+        link: "",
+        place: "Title",
+        price: "1800 $"
+    },
+    {
+        image: "src/Home/Assets/main/Scroll/valley-4646114_1280.jpg",
+        alt: "Valley img",
+        link: "",
+        place: "Title",
+        price: "2200 $"
+    },
+    
+];
+export default CardScroll;

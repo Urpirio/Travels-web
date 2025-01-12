2a9474955846cc1ef1534b9fe305acd2b7770224
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-11 21:26:35 -0400

    Se agregaron nuevas sections.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,9 @@
+import boxicons from "boxicons";
 import SectionMain1 from "./Home/Components/main/SectionMain1";
 import SectionMain2 from "./Home/Components/main/SectionMain2";
 import SectionMain3 from "./Home/Components/main/SectionMain3";
+import SectionMain4 from "./Home/Components/main/SectionMain4";
+import SectionMain5 from "./Home/Components/main/SectionMain5";
 const Home = () =>{
     return(
         <section>
@@ -11,6 +14,8 @@ const Home = () =>{
                 <SectionMain1/>
                 <SectionMain2/>
                 <SectionMain3/>
+                <SectionMain4/>
+                <SectionMain5/>
             </main>
             <footer>
 

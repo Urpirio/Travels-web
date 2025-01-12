7d561e61281640e45f71704a8de41018932dddba
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-11 21:28:36 -0400

    Se agrego una Array de datos generales.

---
+++ src/Home/Components/main/data/GeneralImg.js
@@ -0,0 +1,8 @@
+const Generalimg = [
+    {
+        SM3img:"src/Home/Assets/main/ama-dablam-2064522_1920.jpg",
+        SM4img: "src/Home/Assets/main/lake-6701636_1920.jpg",
+
+    },
+];
+export default Generalimg;

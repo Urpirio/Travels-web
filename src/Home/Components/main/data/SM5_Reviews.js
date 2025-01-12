c604c08cda9a27481b4deeab4f5c91bf5a1ba207
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-11 21:29:53 -0400

    Se agrego el componentes de datos de  la section 5 de las review.

---
+++ src/Home/Components/main/data/SM5_Reviews.js
@@ -0,0 +1,27 @@
+const ReviewsData = [
+    {
+        id: "CardR1",
+        name:"Luisa Moreno",
+        date: "2024-12-09",
+        Comment: " ullamcorper. Maecenas mollis ligula non dui imperdiet, a bibendum ex accumsan. Donec rutrum varius massa, quis consequat turpis elementum in. Sed sit amet congue nisl. Cras sed lobortis magna. ",
+        Image: "src/Home/Assets/main/Review/image (2).png",
+        Dimage: "Luisa Moreno img",
+    },
+    {
+        id: "CardR2",
+        name:"Nashla Sena",
+        date: "2024-12-27",
+        Comment: "  ullamcorper. Maecenas mollis ligula non dui imperdiet, a bibendum ex accumsan. Donec rutrum varius massa, quis consequat turpis elementum in. Sed sit amet congue nisl. Cras sed lobortis magna.",
+        Image: "src/Home/Assets/main/Review/image.png",
+        Dimage: "Nashla Sena img",
+    },
+    {
+        id: "CardR3",
+        name:"Maria Juaquina",
+        date: "2024-12-19",
+        Comment: "  ullamcorper. Maecenas mollis ligula non dui imperdiet, a bibendum ex accumsan. Donec rutrum varius massa, quis consequat turpis elementum in. Sed sit amet congue nisl. Cras sed lobortis magna.",
+        Image: "src/Home/Assets/main/Review/image (1).png",
+        Dimage: "Maria Juaquina img",
+    },
+];
+export default ReviewsData;

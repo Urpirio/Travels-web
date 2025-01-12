dbff571b509eb923d222cfd868e1fd5bef92090f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-11 21:32:23 -0400

    Se creo la section 4P1

---
+++ src/Home/Components/main/SectionMain4.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import Generalimg from './data/GeneralImg';
+import "./style/SectionM4.css";
+import SectionMain4P2 from './SectionMain4P2';
+
+export default class SectionMain4 extends Component {
+
+  render() {
+    
+    const GeneralImgList = Generalimg.map(
+        GT =>{
+            return(
+                <img src={GT.SM4img} alt="Montain" />
+            )
+        }
+    );
+
+
+    return (
+      <section className='SectionMain4'>
+        <div className='SectionMain4-div1'>
+            <div>
+                <div></div>
+            </div>
+            <h1>Why Us?</h1>
+        </div>
+        <div className='SectionMain4-div2'>
+            <div className='SectionMain4-div1-div1'>
+                <h1>Hobbiton</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, quia laudantium ipsa accusamus quas quae harum,
+                     nemo distinctio,aperiam provident asperiores voluptates perspiciatis nobis amet nisi? Animi sit similique molestias!</p>
+                
+                <button>Read More</button>
+            </div>
+            <div className='SectionMain4-div1-div2'>
+                {GeneralImgList}
+            </div>
+            <SectionMain4P2/>
+        </div>
+      </section>
+    )
+  }
+};

0a1d7ee282cd859f756631ded6dee7f58cf9895f
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-12 20:04:58 -0400

    Se agrego la nueva section 6 del  main y la nueva section 1 del footer

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,6 +4,8 @@ import SectionMain2 from "./Home/Components/main/SectionMain2";
 import SectionMain3 from "./Home/Components/main/SectionMain3";
 import SectionMain4 from "./Home/Components/main/SectionMain4";
 import SectionMain5 from "./Home/Components/main/SectionMain5";
+import SectionMain6 from "./Home/Components/main/SectionMain6";
+import SectionFooter1 from "./Home/Components/footer/SectionFooter1";
 const Home = () =>{
     return(
         <section>
@@ -16,9 +18,10 @@ const Home = () =>{
                 <SectionMain3/>
                 <SectionMain4/>
                 <SectionMain5/>
+                <SectionMain6/>
             </main>
             <footer>
-
+                <SectionFooter1/>
             </footer>
         </section>
     )

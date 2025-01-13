ae245b33632ba8082818b58be7e1b53385e830de
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-12 21:36:04 -0400

    Se creo el componente del header

--- src/Home/Components/Header/SectionHeader1.jsx
+++ src/Home/Components/Header/SectionHeader1.jsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import InfoNav from '../main/data/SM1_InfoNav';
+import "./style/SectionH1.css"
 
 export default class SectionHeader1 extends Component {
   render() {
+
+    const Nav = (nav) =>{
+        return(
+            <a href={nav.link}>{nav.name}</a>
+        )
+    };
+
+    const Navlist = InfoNav.map( N => {
+        return(
+            <Nav link ={N.link} name = {N.name} />
+        )
+    });
+
+   
+
     return (
-      <section>
-        
+      <section className='SectionHeader1' id='SectionHeader1'>
+        <nav>
+        {Navlist}
+        </nav>
       </section>
     )
   }

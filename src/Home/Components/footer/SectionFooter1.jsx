b233d9d56c074e732467b0be594d2ac35f6c2c68
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-01-12 20:05:44 -0400

    Se creo la section 1  del footer.

---
+++ src/Home/Components/footer/SectionFooter1.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./style/SectionF.css";
+
+export default class  extends Component {
+  render() {
+    return (
+      <section className='SectionFooter1'>
+        <div>
+            <h1>By<span>UrpirioDev</span></h1>
+        </div>
+      </section>
+    )
+  }
+}
